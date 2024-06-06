bfa094961e758715bfb8bb432a65d833e8491324
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-06 20:20:49 +0200

    variables

---
+++ variables/variables.js
@@ -0,0 +1,51 @@
+//Var
+var favoriteFood = 'pizza';
+var numOfSlices = 8
+
+console.log(favoriteFood);
+console.log(numOfSlices)
+
+
+
+//Let
+let changeMe = true;
+changeMe = false;
+
+console.log(changeMe);
+
+
+
+//Const
+const entree = 'Enchiladas';
+// entree='Tacos;' <-- would not work
+console.log(entree);
+
+
+
+//Mathematical Assignment Operators
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+
+
+//++ and --
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar --;
+
+console.log(gainedDollar);
+console.log(lostDollar);
