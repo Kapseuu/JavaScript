2fcbf87c934c0b4dfbec4ee41723e0d8add80b1a
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-06 20:02:03 +0200

    console log basics

---
+++ console log basics/console log basics.js
@@ -0,0 +1,10 @@
+console.log(2024);
+console.log('Hello World!');
+console.log('Hello' + ' ' + 'World' + '!');
+console.log('Hello' + ' World' + '!');
+console.log(10 + 21.5);
+console.log(10 - 19.49);
+console.log(10 * 0.12);
+console.log(10 / 5);
+console.log('Hello'.length);
+console.log('Teaching the world how to code'.length)
