7f6ac218eada22aafd19b0ef7efd2465efa53685
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-08 13:34:56 +0200

    conditional statement

---
+++ conditional statements/if statement.js
@@ -0,0 +1,24 @@
+// if/else
+
+let sale = true;
+
+if (sale===true){
+  console.log('Time to buy!')
+} //console will output 'Time to buy!'
+
+if (sale===false){
+    console.log('Time to buy!')
+  } //console will not output 'Time to buy!'
+
+
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else{
+  console.log('Time to wait for a sale.')
+}//console will output 'Time to wait for a sale.'
+
+
+
