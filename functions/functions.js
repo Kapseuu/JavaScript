d99a931bbb1924c6451f05979dc8bf31ec627a12
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-09 20:28:00 +0200

    functions

---
+++ functions/functions.js
@@ -0,0 +1,19 @@
+//Function Declarations
+function getReminder(){
+    console.log("Water the plants");
+  }
+  
+  function greetInSpanish(){
+    console.log("Buenas tardes.");
+  }
+
+
+
+//Calling a Function
+function sayThanks(){
+    console.log("Thank you for your purchase! We appreciate your business.");
+  }
+  
+  sayThanks();
+  sayThanks();
+  sayThanks(); //you can call functions as many times as you want them
