7cf05ebe248b6072360b79d584f482a9e1c810c0
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-10 17:19:05 +0200

    arrays

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -71,3 +71,30 @@ console.log(groceryList);
 
 const pastaIndex = groceryList.indexOf("pasta");
 console.log(pastaIndex);
+
+
+
+//Arrays and Functions
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept);
+
+function removeElement(newArr){
+  newArr.pop()
+}
+
+removeElement(concept)
+console.log(concept)
+
+
+
+//Nested Arrays
+let numberClusters = [[1, 2], [3, 4], [5, 6]];
+
+const target = numberClusters[2][1];
+console.log(target);
