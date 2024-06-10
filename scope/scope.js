639f8f8072bf1b3d8c65b10d30ba277bb684a520
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-10 16:44:57 +0200

    scope

--- scope/scope.js
+++ scope/scope.js
@@ -30,3 +30,18 @@ let logVisibleLightWaves = () => {
   
 logVisibleLightWaves();
 //console.log(lightWaves); <-- It will output error
+
+
+
+//Scope Pollution
+/*const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {*/
+  stars="Sirius";
+/*	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());*/
+console.log(stars);
