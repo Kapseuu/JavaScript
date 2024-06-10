fcf476d2be1abebfbc88f18108e44503c8b6122e
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-10 16:37:48 +0200

    scope

--- scope/scope.js
+++ scope/scope.js
@@ -10,3 +10,16 @@ console.log(logCitySkyline());
 
 
 //Global Scope
+let satellite = "The Moon";
+let galaxy = "The Milky Way";
+let stars = "North Star";
+
+let callMyNightSky = () => {
+  return "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+}
+
+console.log(callMyNightSky());
+
+
+
+//
