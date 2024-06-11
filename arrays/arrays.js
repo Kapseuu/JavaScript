61c50ba0cbc618148585ca5befa48e450f41d6af
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-11 17:29:20 +0200

    arrays

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -98,3 +98,14 @@ let numberClusters = [[1, 2], [3, 4], [5, 6]];
 
 const target = numberClusters[2][1];
 console.log(target);
+
+
+
+//The For Loop
+for(let loop = 5; loop <= 10; loop++){ //loop from 5 to 10
+  console.log(loop)
+}
+
+
+
+//
