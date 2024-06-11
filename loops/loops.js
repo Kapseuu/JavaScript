4273f47c63f59e02dfe344f4610e75a79f425fd0
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-11 17:32:34 +0200

    loops

---
+++ loops/loops.js
@@ -0,0 +1,12 @@
+
+//The For Loop
+for(let loop = 5; loop <= 10; loop++){ //loop from 5 to 10
+    console.log(loop)
+}
+  
+  
+  
+//Looping in Reverse
+for (let counter = 3; counter >= 0; counter--){
+    console.log(counter);
+}
