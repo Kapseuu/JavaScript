300c98433213d48a89dbe37940a6c38462a9c832
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-11 17:58:11 +0200

    loops

--- loops/loops.js
+++ loops/loops.js
@@ -35,3 +35,38 @@ for(let i = 0; i < bobsFollowers.length; i++){
   }
 }
 console.log(mutualFollowers);
+
+
+
+//The while loop
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+let currentCard;
+while(currentCard !== "spade"){
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
+
+
+
+//Do...While Statements
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+
+do{
+  cupsAdded++;
+  console.log(cupsAdded);
+}while(cupsAdded < cupsOfSugarNeeded);
+
+
+
+//The break Keyword
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for(let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === "Notorious B.I.G."){
+    break;
+  }
+}
+console.log("And if you don't know, now you know.")
