6afd039bbe54b651d6c8de78f64f4df02859fc3f
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-12 17:52:51 +0200

    objects

--- objects/objects.js
+++ objects/objects.js
@@ -6,4 +6,20 @@ let fasterShip = {
 
 
 
-//
+//Accessing Properties
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+
+let crewCount = spaceship.numCrew;
+console.log(crewCount);
+  
+let planetArray = spaceship.flightPath;
+console.log(planetArray);
+
+
+
