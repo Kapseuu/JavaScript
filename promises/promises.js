f6b3092ec1f43f2521176208371d41896dd90d52
Kapseuu <Kapseu.poczta@gmail.com>
2024-06-17 16:52:47 +0200

    promises

--- promises/promises.js
+++ promises/promises.js
@@ -71,3 +71,27 @@ const handleFailureCatch = (rejectReason) => {
 checkInventory(orderCatch)
 	.then(handleSuccessCatch)
 	.catch(handleFailureCatch);
+
+
+
+//Chaining Multiple Promises
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const orderMultiple = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+checkInventory(orderMultiple)
+.then((resolvedValueArray) => {
+ return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
